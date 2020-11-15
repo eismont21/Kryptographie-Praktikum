6a147d754928efb690f6d9680cfdd5f316757130
yayapa <dimaseletkov@gmail.com>
2020-11-15 23:28:26 +0100

    first version doesnt work

--- framework/permutation/attacke.c
+++ framework/permutation/attacke.c
@@ -72,7 +72,65 @@ int main (void)
 }
 
 
+int get_distribution(char u, char v) {
+    if ((u == '.' || u == ',' || u == ')') && isspace(v)){
+        return 1;
+    }
+    if (isspace(v) && ((v >= 'A' && v <='Z') || (v == '('))) {
+        return 1;
+    }
+    return 0;
+}
 
+int find_max(int* a, int n) {
+    int max = a[0];
+    int max_index = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+            max_index = i;
+        }
+    }
+    return max;
+}
+
+int find_index_min(int* a, int n) {
+    int min = a[0];
+    int min_index = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] < min) {
+            min = a[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+void quicksort(int *mas, int* permuts, int first, int last)
+{
+    int mid, count;
+    int f=first, l=last;
+    mid=mas[(f+l) / 2]; //вычисление опорного элемента
+    do
+    {
+        while (mas[f]<mid) f++;
+        while (mas[l]>mid) l--;
+        if (f<=l) //перестановка элементов
+        {
+            count=mas[f];
+            mas[f]=mas[l];
+            mas[l]=count;
+
+            count = permuts;
+            permuts[f] = permuts[l];
+            permuts[l] = count;
+
+            f++;
+            l--;
+        }
+    } while (f<l);
+    if (first<l) quicksort(mas, permuts, first, l);
+    if (f<last) quicksort(mas, permuts, f, last);
+}
 void attacke (void)
 {
 	/* *** Hier soll die Attacke implementiert werden *** */
@@ -89,6 +147,25 @@ void attacke (void)
 	*/
 
   /* Aufgabe */
+  int number_blocks = laenge / PERIODE;
 
+  int A[PERIODE][PERIODE];
+  for (int i = 0; i < PERIODE; i++) {
+      for (int j = 0; j < PERIODE; j++) {
+          A[i][j] = 0;
+          //if not diagonal
+          if (i != j) {
+              for (int k = 0; k < number_blocks; k++)
+                  A[i][j] += get_distribution(chiffrat[PERIODE*k + i], chiffrat[PERIODE*k + j]);
+          }
+      }
+  }
+  int b[PERIODE];
+  //Suche Maximum in jeder Zeile
+  for (int i = 0; i < PERIODE; i++) {
+      b[i] = find_max(b[i], PERIODE);
+  }
+  for (int i = 1; i <= PERIODE; i++) loesung[i] = i;
+  quicksort(b, loesung, 0, PERIODE-1);
 }
 
