05d7ae21df2ac1ed8665cbdd557024a8a2e92dde
eismont <eismont.ar@gmail.com>
2021-02-07 15:48:56 +0100

    Update ecc_client.c

--- framework/ecc_invalid/ecc_client.c
+++ framework/ecc_invalid/ecc_client.c
@@ -7,6 +7,13 @@ Connection con;
 
 mpz_t p,a,b;
 
+static void factorial (long n, mpz_t r)
+{
+    mpz_init_set_si (r, 1);
+    for (; n > 1; n--) {
+        mpz_mul_si (r, r, n);
+    }
+}
 void validate_points(void){
   ecc_point pn;mpz_init(pn.x);mpz_init(pn.y);pn.inf=0;
   for(int pnt = 0; invalid_points[pnt].prime != 0; pnt++){
@@ -43,8 +50,10 @@ void submit_solution(mpz_t key){
   if(rep.state == 0){
     const char *date = Now();
     printf("Solution submitted at %s was correct.\n", date);
+    exit(1);
   }else{
     printf("Solution was incorrect.\n");
+
   }
 }
 
@@ -72,13 +81,28 @@ int main(int argc, char *argv[]){
 
     con = ConnectTo(MakeNetName(NULL), "ECC_invalid_Daemon");
     uint8_t cipher[32];
-    uint8_t res[32];
+    uint8_t cipher_text[16];
+    uint8_t iv[16];
+    uint8_t res[16];
+    mpz_t solution;
+    mpz_init(solution);
+    char* solution_str;
+    mpz_t two;
+    mpz_init_set_ui(two, 2);
+    mpz_t pi; mpz_init_set_ui(pi, 1);
+
     ecc_point pn;mpz_init(pn.x);mpz_init(pn.y);pn.inf=0;
     for(int pnt = 0; invalid_points[pnt].prime != 0; pnt++) {
+        //if (invalid_points[pnt].prime < 50) continue;
+        printf("\t\tPrime is %d\n", invalid_points[pnt].prime);
         mpz_set_str(pn.x, invalid_points[pnt].px, 16);
         mpz_set_str(pn.y, invalid_points[pnt].py, 16);
+        mpz_mul_ui(pi, pi, invalid_points[pnt].prime);
+
 
         for (int i = 1; i <= invalid_points[pnt].prime; i++) {
+            //mpz_set_ui(pi, i);
+            //factorial(i, pi);
             ecc_point dbl;mpz_init(dbl.x);mpz_init(dbl.y);dbl.inf=0;
             mpz_set(dbl.x, pn.x);
             mpz_set(dbl.y, pn.y);
@@ -86,25 +110,44 @@ int main(int argc, char *argv[]){
 
             mpz_t k; mpz_init(k);
             mpz_set_ui(k, i);
+            //k*dbl
             ecc_dbl_and_add(&dbl, dbl, k, a, p);
 
             test_connection(cipher, dbl);
             aeskey key;
             key = aeskey_from_ec(dbl);
-            aes_dec(res, cipher, key);
-            //res = res ^ key;
-            size_t key_size = sizeof(key.key) / sizeof(key.key[0]);
-            int j_key = 0;
-            for (int j = 0; j < 32; j++) {
-                if (j_key == key_size) {
-                    j_key = 0;
-                }
-                res[j] = res[j] ^ key.key[j_key];
-                j_key++;
+
+            for (int i = 16; i < 32; i++) {
+                cipher_text[i-16] = cipher[i];
             }
-            for (int j = 0; j < 32; j++) printf("%d", res[i]);
+            for (int i = 0; i < 16; i++) {
+                iv[i] = cipher[i];
+            }
+            //decryption AES
+            aes_dec(cipher_text, res, key);
+
+            //CBC Modus
+            for (int i = 0; i < 16; i++) {
+                res[i] = res[i] ^ iv[i];
+            }
 
 
+            //printf("cipher: \n");
+            //for (int j = 0; j < 32; j++)  printf("%d", res[i]);
+            int index = 0;
+            for (int i=0; i<16; i++)
+                index += sprintf(&solution_str[index], "%d", res[i]);
+
+            if (solution_str[0] != '0' || solution_str[1] != '0' || solution_str[2] != '0') continue;
+            printf("\ncipher_str: %s\n", solution_str);
+
+            mpz_set_str(solution, solution_str, 16);
+            mpz_powm(solution, solution, two, pi);
+            mpz_sqrt(solution, solution);
+            gmp_printf("Solution =  %Zd\n", solution);
+
+            submit_solution(solution);
+            con = ConnectTo(MakeNetName(NULL), "ECC_invalid_Daemon");
         }
     }
   // Task: Implement the attack to recover the daemon's private key
